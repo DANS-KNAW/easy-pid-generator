8c553db45743046469dd3d885ac42c54aac9db1a
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-09-07 15:09:43 +0200

    PidGeneratorService

---
+++ src/main/scala/nl/knaw/dans/easy/pid/microservice/PidGeneratorService.scala
@@ -0,0 +1,56 @@
+package nl.knaw.dans.easy.pid.microservice
+
+import java.io.File
+import java.util.UUID
+
+import com.hazelcast.core.HazelcastInstance
+import com.typesafe.config.ConfigFactory
+import nl.knaw.dans.easy.pid.{PidGenerator, RanOutOfSeeds}
+import org.slf4j.LoggerFactory
+
+import scala.util.{Failure, Success, Try}
+
+object PidGeneratorService {
+
+  val log = LoggerFactory.getLogger(getClass)
+
+  val home = new File(System.getenv("EASY_PID_GENERATOR_HOME"))
+  val conf = ConfigFactory.parseFile(new File(home, "cfg/application.conf"))
+  // TODO refactor to the other parsing library
+  // TODO refactor to parsing the config to a Settings or Parameters object
+
+  val urns = PidGenerator.urnGenerator(conf, home)
+  val dois = PidGenerator.doiGenerator(conf, home)
+
+  def run(implicit hz: HazelcastInstance) = {
+    hz.getQueue[String]("pid-inbox")
+      .observe()
+      .map(JSON.parseRequest _ andThen executeRequest)
+      .subscribe(response => send(response))
+  }
+
+  def executeRequest(request: RequestMessage): Response = {
+    val RequestMessage(RequestHead(uuid, responseDS), RequestBody(pidType)) = request
+
+    def respond(result: Try[String]): ResponseResult =
+      result match {
+        case Success(pid) => ResponseSuccessResult(pid)
+        case Failure(RanOutOfSeeds()) => ResponseFailureResult("No more identifiers")
+        case Failure(_) => ResponseFailureResult("Error when retrieving previous seed or saving current seed")
+      }
+
+    val result: ResponseResult = pidType match {
+      case URN => respond(urns.next())
+      case DOI => respond(dois.next())
+    }
+
+    (uuid, responseDS, ResponseMessage(ResponseHead(uuid), ResponseBody(pidType, result)))
+  }
+
+  def send(response: Response)(implicit hz: HazelcastInstance): Unit = {
+    val (uuid, responseDS, message) = response
+    val ds = hz.getMap[UUID, String](responseDS)
+    val json = JSON.writeResponse(message)
+    ds.put(uuid, json)
+  }
+}
--- src/main/scala/nl/knaw/dans/easy/pid/microservice/ServiceStarter.scala
+++ src/main/scala/nl/knaw/dans/easy/pid/microservice/ServiceStarter.scala
@@ -1,11 +1,16 @@
 package nl.knaw.dans.easy.pid.microservice
 
+import com.hazelcast.Scala.client._
+import com.hazelcast.Scala.serialization
+import com.hazelcast.client.config.ClientConfig
+import com.hazelcast.core.HazelcastInstance
 import org.apache.commons.daemon.{Daemon, DaemonContext}
 import org.slf4j.LoggerFactory
 
 class ServiceStarter extends Daemon {
 
   val log = LoggerFactory.getLogger(getClass)
+  var hz: HazelcastInstance = _
 
   def init(context: DaemonContext): Unit = {
     log.info("Initializing pid-generator service ...")
@@ -13,7 +18,12 @@ class ServiceStarter extends Daemon {
 
   def start(): Unit = {
     log.info("Starting pid-generator service ...")
-    // TODO run service
+
+    val conf = new ClientConfig()
+    serialization.Defaults.register(conf.getSerializationConfig)
+    hz = conf.newClient()
+
+    PidGeneratorService.run(hz) // can't pass this implicitly since `hz` is a variable
   }
 
   def stop(): Unit = {
