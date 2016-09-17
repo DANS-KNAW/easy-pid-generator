9f42366027720c7fe2d68bf938f8b286c538d659
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2016-09-17 19:20:31 +0200

    Something went wrong with a rename earlier, fixed it

--- src/main/scala/nl/knaw/dans/easy/pid/PidService.scala
+++ src/main/scala/nl/knaw/dans/easy/pid/PidService.scala
@@ -32,7 +32,7 @@ class PidService extends ScalatraServlet with ScalateSupport {
   val urns = PidGenerator.urnGenerator(conf, home)
   val dois = PidGenerator.doiGenerator(conf, home)
 
-  log.info("PID Generator nl.knaw.dans.easy.pid.Service running ...")
+  log.info("PID Generator Service running ...")
       
   get("/") {
     Ok("Persistent Identifier Generator running")
