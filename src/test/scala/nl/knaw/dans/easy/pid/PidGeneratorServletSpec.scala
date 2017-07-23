ae8c4b83a479e050f07cda635897dabf0b58d323
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-07-23 18:14:35 +0200

    WIP

--- src/test/scala/nl/knaw/dans/easy/pid/PidGeneratorServletSpec.scala
+++ src/test/scala/nl/knaw/dans/easy/pid/PidGeneratorServletSpec.scala
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.knaw.dans.easy.pid.service
+package nl.knaw.dans.easy.pid
 
-import nl.knaw.dans.easy.pid._
 import nl.knaw.dans.easy.pid.fixture.{ ConfigurationSupportFixture, SeedDatabaseFixture, ServletFixture, TestSupportFixture }
 import org.scalamock.scalatest.MockFactory
 import org.scalatra.test.scalatest.ScalatraSuite
