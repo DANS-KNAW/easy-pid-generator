a0388087cf7ec3ca128f4c8a4e95a634782be5c8
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-12-05 14:31:18 +0100

    rollback in db generation script

--- src/test/resources/database/generateHsqldbScripts.sh
+++ src/test/resources/database/generateHsqldbScripts.sh
@@ -8,26 +8,48 @@ SQLTOOLJAR=$LIB/sqltool.jar
 HSQLDBJAR=$LIB/hsqldb.jar
 RC=$LIB/db.rc
 
+DB_PROPS=$ROOT/db.properties
+DB_SCRIPT=$ROOT/db.script
+
 DB_PROPS_BACKUP=$ROOT/db-$(date  +"%Y-%m-%dT%H:%M:%S").properties
 DB_SCRIPT_BACKUP=$ROOT/db-$(date  +"%Y-%m-%dT%H:%M:%S").script
 
-mv $ROOT/db.properties $DB_PROPS_BACKUP
-mv $ROOT/db.script $DB_SCRIPT_BACKUP
+# rename the original files to put them back later, if necessary
+mv $DB_PROPS $DB_PROPS_BACKUP
+mv $DB_SCRIPT $DB_SCRIPT_BACKUP
 
 echo 'downloading tools'
+# download necessary libraries
 mkdir $LIB
 curl -s http://central.maven.org/maven2/org/hsqldb/sqltool/2.4.1/sqltool-2.4.1.jar > $SQLTOOLJAR
 curl -s http://central.maven.org/maven2/org/hsqldb/hsqldb/2.4.1/hsqldb-2.4.1.jar > $HSQLDBJAR
 
+# write the db.rc file
 echo 'urlid pidgen' > $RC
 echo 'url jdbc:hsqldb:file:'$ROOT'/db;shutdown=true' >> $RC
 
 echo 'generating database scripts'
-# TODO rollback on failure
-java -jar $SQLTOOLJAR --rcFile=$RC pidgen $ROOT/database.sql || exit 1
+# generate new database files
+java -jar $SQLTOOLJAR --rcFile=$RC pidgen $ROOT/database.sql
 
-rm -rf $LIB
-rm $DB_PROPS_BACKUP
-rm $DB_SCRIPT_BACKUP
+if [[ $? -eq 0 ]]; then
+    # remove lib directory and the original files that now have been replaced
+    rm -rf $LIB
+    rm $DB_PROPS_BACKUP
+    rm $DB_SCRIPT_BACKUP
 
-echo 'new database script files are generated'
+    echo 'new database script files are generated'
+else
+    echo 'generating database failed'
+
+    # remove lib directory and newly generated files
+    rm -rf $LIB
+    rm $DB_PROPS
+    rm $DB_SCRIPT
+
+    # put back the original files
+    mv $DB_PROPS_BACKUP $DB_PROPS
+    mv $DB_SCRIPT_BACKUP $DB_SCRIPT
+
+    echo 'changes have been rolled back'
+fi
